ead9e2551d8bd9080316392569f553d8ddabf336
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-07-25 19:08:04 -0400

    step 2 and 3 finished

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -17,8 +17,10 @@ const useDarkMode = () => {
   const [mode, setMode] = useLocalStorage('key1');
 
   useEffect( () => {
-
     return mode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
-  }, []);
+  }, [mode]);
 
+  return [mode, setMode];
 }
+
+export default useDarkMode;
